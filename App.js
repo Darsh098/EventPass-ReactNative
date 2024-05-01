e79fc0bbeb8269152b8fd87d85605496858782c9
Darsh <darshdoshi01@gmail.com>
2024-05-01 15:43:59 +0530

    Clerk Component Added

--- App.js
+++ App.js
@@ -1,15 +1,27 @@
 import { StyleSheet, Text, View } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import MainRoute from "./src/MainRoute";
+// import MainRoute from "./src/MainRoute";
 import { NavigationContainer } from "@react-navigation/native";
+import Constants from "expo-constants";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <MainRoute />
-    </NavigationContainer>
+    <ClerkProvider
+      publishableKey={Constants.expoConfig.extra.clerkPublishableKey}
+    >
+      <NavigationContainer>
+        {/* <MainRoute /> */}
+        <SignedIn>
+          <Text>You are Signed in</Text>
+        </SignedIn>
+        <SignedOut>
+          <Text>You are Signed out</Text>
+        </SignedOut>
+      </NavigationContainer>
+    </ClerkProvider>
   );
 }
 
---
+++ app.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "event-pass",
+  version: "1.0.0",
+  extra: {
+    clerkPublishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+  },
+};
